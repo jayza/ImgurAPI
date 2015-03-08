8ecbd05c0e905440207b12e3c8a3736a199cc93e
jza <jake@kirkandstanley.se>
2015-03-08 19:18:20 +0100

    Added data parameter to delete method.

--- vendor/src/Request.php
+++ vendor/src/Request.php
@@ -2,6 +2,7 @@
 class Request {
   /**
   * @var string $access_token The access token that is given from the Session class
+  * @var object $response Contains the response from a request.
   */
   private $access_token;
   private $response;
@@ -92,9 +93,10 @@ class Request {
   /**
   * Function to make a DELETE request to the Imgur REST API.
   * @param string $endpoint API Endpoint from the Imgur API.
+  * @param array $data Array of data to send in the DELETE request.
   * @return Request Object contains the response data from Imgur (see Imgur Developer Documentation).
   */
-  public function delete($endpoint) {
+  public function delete($endpoint, $data) {
     $headers = array(
       'Authorization: Bearer ' . $this->access_token,
     );
@@ -102,6 +104,7 @@ class Request {
     $ch = curl_init(Settings::getPublic('api_url') . $endpoint);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     $response = json_decode(curl_exec($ch));
     curl_close($ch);
